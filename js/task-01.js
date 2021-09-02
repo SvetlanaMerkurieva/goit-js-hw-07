3b3eac45ac230e8a03409087414d16251c0e2d05
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-09-02 13:30:32 +0300

    Решение задания № 1

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
-const itemEl = document.querySelectorAll(".item");
-console.log(itemEl);
-console.log(`В списке ${itemEl.length} категории`);
+const itemsEl = document.querySelectorAll(".item");
+console.log(`В списке ${itemsEl.length} категории`);
+
+    const listCategories = itemsEl.forEach
+(item => {
+    console.log(`Категория: ${item.firstElementChild.textContent}`);
+    console.log(`Количество элементов: ${item.lastElementChild.children.length}`);
+},
+);
