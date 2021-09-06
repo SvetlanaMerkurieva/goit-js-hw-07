3cda9f9927ae9431943b3dc2a0e958860663f4c5
SvetlanaMerkurieva <merkureva83@gmail.com>
2021-09-06 20:16:47 +0300

    Решение задания № 5

---
+++ js/task-05.js
@@ -0,0 +1,9 @@
+const inputEl = document.querySelector("#name-input");
+const spanEl = document.querySelector("#name-output");
+
+const onNameInput = inputEl.addEventListener("input", (event) => {
+    spanEl.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        spanEl.textContent = "незнакомец";
+    };
+});
