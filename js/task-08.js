dc6b339ef975915cb39c29e0a885d134c11f6209
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-09-09 16:43:43 +0300

    Решение задание № 8

--- js/task-08.js
+++ js/task-08.js
@@ -3,32 +3,29 @@ const btnCreateEl = document.querySelector('[data-action="render"]');
 const btnClearEl = document.querySelector('[data-action="destroy"]');
 const mainDivEl = document.querySelector("#boxes");
 
-
-function getBackgroundColor() {
-    const r = Math.floor(Math.random() * 256)
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return [r, g, b];
-}
-
-inputEl.addEventListener("input", () => {
+inputEl.addEventListener("input", () => {});
     function onCreateDivs() {
         const divsArr = [];
-        let widthDiv = 30;
-        let heightDiv = 30;
-
         for (let i = 0; i < Number(inputEl.value); i += 1) {
+            function getRandomColor(max) {
+                return Math.floor(Math.random() * Math.floor(max))
+            };
+                let r = getRandomColor(255);
+                let g = getRandomColor(255);
+                let b = getRandomColor(255);
+            
             divsArr.push(document.createElement("div"));
-            divsArr[i].style.backgroundColor = "red";
-            divsArr[i].style.width = "30px" /*`${widthDiv} + 10`*/;
-            divsArr[i].style.height = "30px" /*`${heightDiv} + 10`*/;
+            divsArr[i].style.backgroundColor = "rgb(" + r + "," + g + "," + b + ")";
+            divsArr[i].style.width = `${30 + i * 10}px`;
+            divsArr[i].style.height = `${30 + i * 10}px`;
             divsArr[i].style.marginTop = "10px";
         };
         mainDivEl.append(...divsArr);
         console.log(inputEl.value);
     };
     btnCreateEl.addEventListener("click", onCreateDivs);
-});
+
 btnClearEl.addEventListener("click", () => {
     mainDivEl.innerHTML = "";
+    inputEl.value = "";
 });
